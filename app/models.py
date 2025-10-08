34edbeecd95a006a41c64e257adbe34e81675466
Ramya27012004 <ramyapalle129@gmail.com>
2025-10-08 19:27:29 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Dept(models.Model):
+    deptno=models.IntegerField(max_length=10,primary_key=True)
+    dname=models.CharField(max_length=20,unique=True)
+    loc=models.CharField(max_length=20)
+    def __str__(self):
+        return self.dname
+
+class Emp(models.Model):
+    empno=models.IntegerField(max_length=10)
+    ename=models.CharField()
+    job=models.CharField()
+    mgr=models.ForeignKey('self',on_delete=models.SET_NULL,null=True,blank=True)
+    hiredate=models.DateField()
+    sal=models.DecimalField(max_digits=8,decimal_places=3)
+    comm=models.DecimalField(max_digits=8,decimal_places=4 ,null=True, blank=True)
+    deptno=models.ForeignKey(Dept,on_delete=models.CASCADE,null=True,blank=True)
+    def __str__(self):
+        return self.ename
---
+++ app/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from app.models import *
+def inset_dept(requst):
+    dno=int(input('enter the deptno'))
+    dname=input('enter the dname')
+    loc=input('enter the loc')
+    TDO=Dept.objects.get_or_create(deptno=dno,dname=dname,loc=loc)
+    if TDO[1]:
+        return HttpResponse('Dept is created')
+    else:
+        return HttpResponse('dept is alredy exit')
+def insert_emp(request):
+    empno=int(input('enter the empno:'))
+    ename=input('enter the ename:')
+    job=input('enter the job:')
+    meno=input('enter the mgr:')
+    if meno:
+        MEO=Emp.objects.get(empno=int(meno))
+    else:
+        MEO=None
+    hiredate=input('enter the date:')
+    sal=float(input('enter the sal:'))
+    comm=input('enter the comm:')
+    if comm:
+        comm=float(comm)
+    else:
+        comm=None
+    dn=int(input('enter the number:'))
+    DO=Dept.objects.get(deptno=dn)
+    TEMPO=Emp.objects.get_or_create(empno=empno,ename=ename,job=job,mgr=MEO,hiredate=hiredate,sal=sal,comm=comm,deptno=DO)
+    if TEMPO[1]:
+        return HttpResponse('emp is created')
+    else:
+        return HttpResponse('emp is alredy exit')
